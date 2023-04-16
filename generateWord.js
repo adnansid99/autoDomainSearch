a92f4f8c2d5c7adc4cb0cc2becb397f6f148607d
Adnan Siddiqui <60623435+adnansid99@users.noreply.github.com>
2023-04-16 14:03:31 +0600

    random 3 letter

--- generateWord.js
+++ generateWord.js
@@ -1,8 +1,16 @@
 const randomWords = require("random-words");
 
-function generateWord(length) {
-  const word = randomWords({ exactly: 1, maxLength: length })[0];
+function generateRandomWord() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let word = '';
+
+  for (let i = 0; i < 3; i++) {
+    const randomIndex = Math.floor(Math.random() * alphabet.length);
+    word += alphabet[randomIndex];
+  }
+
   return word + ".com";
 }
+
 
 module.exports = generateWord;
